da3c603ec4f92449fbed6331b6368369411b2389
furkanGit <furkancaliskan9801@gmail.com>
2022-11-21 16:02:28 +0100

    MILESTONE 0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 //Creare l’array di oggetti con le informazioni fornite.
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "SMM",
+        photo: "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "img/scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "img/barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+console.log(teamMembers);
+
+
