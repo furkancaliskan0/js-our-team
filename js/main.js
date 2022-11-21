42ecd8ad958cf3732bbe59e3438326620965e0f2
furkanGit <furkancaliskan9801@gmail.com>
2022-11-21 15:03:52 +0100

    Initial Setup

---
+++ js/main.js
@@ -0,0 +1,3 @@
+//Creare l’array di oggetti con le informazioni fornite.
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
